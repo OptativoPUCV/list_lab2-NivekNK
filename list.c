52262342084fb1b14123bbc82c036fe14dd2838f
Replit user <replituser@example.com>
2021-03-24 15:08:38 +0000

    new try

--- list.c
+++ list.c
@@ -156,7 +156,7 @@ void * popCurrent(List * list)
     }
 
     aux->next = list->current->next;
-    free(list->current);
+    //free(list->current);
     list->current = list->head;
     
     return data;
